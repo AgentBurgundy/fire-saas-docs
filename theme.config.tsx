0a2a593f10b7bcf797031ce2845ad91d7ea7dd9b
ronald-onesta <professional.burgundy@gmail.com>
2024-02-04 01:12:06 -0600

    updated docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>FireSaaS</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/AgentBurgundy/fire-saas",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/DYE67SUpVt",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "FireSaaS - Made with love",
   },
-}
+};
 
-export default config
+export default config;
